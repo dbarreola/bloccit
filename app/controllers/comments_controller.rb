9f3b05ec660ffb6ab6f28fde28a02cb32a69d03f
Diego Beltran <dbarreola@gmail.com>
2014-08-28 14:47:03 +0000

    Fixed little problems with comments views and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,17 +1,18 @@
 class CommentsController < ApplicationController
   def create
-  	@topic = Topic.find(params[:topic_id])
-  	@post = @topic.posts.find(params[:post_id])
-  	@comments = @post.comments
-  	@comment = current_user.comments.build(params[:comment])
-  	@comment.post = @post
-  	if @comment.save
-  		flash[:notice] = "Comment was created."
-  		redirect_to [@topic, @post]
-  	else
-  		flash[:error] = "There was an error saving the comment. Please try again."
-  		render 'posts/show'
-  	end
+    @topic = Topic.find( params[:topic_id] )
+    @post = @topic.posts.find( params[:post_id] )
+    @comments = @post.comments
+    @comment = current_user.comments.build( comment_params )
+    @comment.post = @post
+    
+    authorize @comment
+    if @comment.save
+      flash[:notice] = "Comment was created."
+    else
+      flash[:error] = "There was an error saving the comment. Please try again."
+      redirect_to [@topic, @post]
+    end
   end
 
   def destroy
@@ -27,6 +28,9 @@ class CommentsController < ApplicationController
       flash[:error] = "Comment couldn't be deleted. Try again."
       redirect_to [@topic, @post]
     end
+  end
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
   end
   
 end
