38cf645e3e1075277c11e48e66a6f792806ae6fa
Diego Beltran <dbarreola@gmail.com>
2014-08-30 17:07:50 +0000

    added voting

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Bloccit::Application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
+
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments, only: [:create, :destroy]
+      post '/up-vote' => 'votes#up_vote', as: :up_vote
+      post '/down-vote' => 'votes#down_vote', as: :down_vote
     end
   end
+  
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
