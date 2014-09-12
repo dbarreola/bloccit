d24fb6f8331ea3c5324598f0a583495d597bb403
Diego Beltran <dbarreola@gmail.com>
2014-09-12 23:29:53 +0000

    added popular posts and user ranking

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Bloccit::Application.routes.draw do
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
 
   resources :topics do
-    resources :posts, except: [:index], controller: 'topics/posts' do
-      resources :comments, only: [:create, :destroy]
-      post '/up-vote' => 'votes#up_vote', as: :up_vote
-      post '/down-vote' => 'votes#down_vote', as: :down_vote
-      resources :favorites, only: [:create, :destroy]
-    end
+    resources :posts, except: [:index], controller: 'topics/posts'
   end
+  resources :posts, only: [:index] do
+  resources :comments, only: [:create, :destroy]
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+  post '/down-vote' => 'votes#down_vote', as: :down_vote
+  resources :favorites, only: [:create, :destroy]
   
   get 'about' => 'welcome#about'
 
