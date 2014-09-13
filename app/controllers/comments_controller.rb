7df71ef8d872c02f8170bbb7495b79e6d9cdca0a
Diego Beltran <dbarreola@gmail.com>
2014-09-13 16:36:22 +0000

    Added destroy functionality for comments with AJAX

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,7 @@
 class CommentsController < ApplicationController
+  
+  respond_to :html, :js
+
   def create
     @topic = Topic.find( params[:topic_id] )
     @post = @topic.posts.find( params[:post_id] )
@@ -23,12 +26,16 @@ class CommentsController < ApplicationController
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+    end
+
+    respond_with(@comment) do |format|
+      format.html {redirect_to [@post.topic, @post]}
     end
   end
+
+  
   def comment_params
     params.require(:comment).permit(:body, :post_id)
   end
